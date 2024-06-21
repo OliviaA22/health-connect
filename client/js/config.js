5ac931821bc9eae82b66b3c129a1bfc9b1cb2563
OliviaA22 <olivia_adaugo@yahoo.com>
2024-06-21 09:32:23 +0200

    nextjs

--- client/js/config.js
+++
@@ -1,4 +0,0 @@
-// config.js
-const API_BASE_URL = 'https://health-connect-kyp7.onrender.com';
-
-export default API_BASE_URL;
--- client/js/scripts.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    // Load common partials
-    $("#navbar").load("partials/navbar.html");
-    $("#searchbar").load("partials/searchbar.html");
-
-    // Add any other common global JS functionality here
-});
