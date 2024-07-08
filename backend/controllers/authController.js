d36ded9569d1dddce9d1402c246b306e0cd00728
OliviaA22 <olivia_adaugo@yahoo.com>
2024-07-09 00:01:01 +0200

    ....

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -24,7 +24,7 @@ class AuthController {
         })
         .status(201)
         .json({
-          id: user.id,
+          userId: user.id,
           first_name: user.first_name,
           last_name: user.last_name,
           role: user.role,
